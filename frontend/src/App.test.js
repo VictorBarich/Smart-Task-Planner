be56aa6ee59f4235593c50af084b383824665bb4
Connor Magnuson <ConnorM.9@outlook.com>
2025-10-17 12:03:04 -0500

    Tasks can move between todo and completed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import App from './App';
 import Task from './Task';
+import TaskList from './TaskList';
 
 test('main page renders tasks header', () => {
   render(<App />);
@@ -9,9 +11,32 @@ test('main page renders tasks header', () => {
 });
 
 test('Task renders passed in information', () => {
-  render(<Task index={999} name={"TestABC"} completed={false} />);
+  render(<Task index={999} name={"TestABC"} completed={false} checkboxActionFunction={()=>{}}/>);
   const taskIndex = screen.getByText(/999./i);
   expect(taskIndex).toBeInTheDocument();
   const taskName = screen.getByText(/TestABC/i);
   expect(taskName).toBeInTheDocument();
+});
+
+test('Task checkbox executes action function when checked', async () => {
+  var executed = false;
+
+  const user = userEvent.setup();
+  render(<Task index={999} name={"TestABC"} completed={false} checkboxActionFunction={()=>{executed = true}}/>);
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+  await user.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(executed).toBe(true);
+});
+
+test('Test result of clicking all task checkboxes', async () => {
+  const user = userEvent.setup();
+  render(<TaskList />);
+  const checkboxes = screen.getAllByRole('checkbox');
+  for (const checkbox of checkboxes) {
+    const checked = checkbox.checked;
+    await user.click(checkbox);
+    expect(checkbox.checked).toBe(!checked);
+  }
 });
