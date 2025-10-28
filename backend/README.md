05aa225a898acb43962290db5cde887e3bc77f91
VictorBarich <victor.m.barich@gmail.com>
2025-10-27 19:29:56 -0500

    Added Readme and checks for AI output

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+In order to run this:
+
+1. Make your API key at https://aistudio.google.com/api-keys
+2. Make an env file
+3. Paste your API key into it like this: API_KEY=mykey
+4. Run python main.py
+
+Also not sure what the limits are like for the free tier, so might get weird errors if you run it excessively.
--- backend/ai.py
+++ backend/ai.py
@@ -18,7 +18,11 @@ def ai_reordering(tasks_object):
         prompt += f"{i}. Task Name: {tasks[i].name}. Task Description: {tasks[i].description}\n"
 
     prompt += """Remember to return the tasks in order of priority from most important to least important.
-                Return only a numbered list of task names, nothing else. Do not repeat the prompt."""
+                Return only a numbered list of task names, nothing else. Do not repeat the prompt.
+                Format exactly like this:
+                1. Task 1
+                2. Task 2
+                3. Task 3"""
 
 
 
@@ -32,6 +36,9 @@ def ai_reordering(tasks_object):
     # Reformat the output and set it to the new tasks list
     # print(f"raw output: {raw_output}")
     list_ordered_names = [line.split(". ", 1)[1].strip() for line in raw_output.split("\n") if ". " in line]
+    if (len(list_ordered_names) != len(tasks)):
+        print(f"Provided {len(tasks)} to model but got only {len(list_ordered_names)} back")
+        
     name_task_mapping = {task.name: task for task in tasks}
     tasks_object.tasks = [name_task_mapping[name] for name in list_ordered_names if name in name_task_mapping]
 
