ca6e2f4c2c3719c324169e731f6e232a286f3183
Evan Doubek <edoub@illinois.edu>
2025-10-30 22:42:15 -0500

    Fixed imports appearance

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,15 @@ from task_routes import router
 from tasks import Task, Tasks
 from global_task_manager import task_manager
 from ai import ai_reordering
+import uvicorn
 
 def main():
 
+    # Example task creation
     task1 = Task("Task 1", "This is the first task.")
     task2 = Task("Task 2", "This is the second task.")
 
+    # Adds example tasks to global task manager
     task_manager.add_task(task1)
     task_manager.add_task(task2)
 
@@ -21,7 +24,6 @@ def main():
     task_manager.list_tasks()
 
 if __name__ == "__main__":
-    import uvicorn
 
     main() # Example task_manager manipulation
 
