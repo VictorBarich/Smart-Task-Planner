49144d2de6295e352654a92cc2584a750eafaeb1
Nate Dower <ndowe@illinois.edu>
2025-11-11 22:19:29 -0600

    Added testing for generic tasks in main

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,9 @@ def main():
     task1 = Task("Task 1", "This is the first task.")
     task2 = Task("Task 2", "This is the second task.")
 
+    task1.__set_due_date__("2024-10-01")
+    task2.__set_due_date__("2024-10-05")
+
     task_manager.add_task(task1)
     task_manager.add_task(task2)
 
@@ -24,6 +27,8 @@ def main():
     task_manager.mark_task_complete(task2)
     print("After marking Task 2 as complete:")
     task_manager.list_tasks()
+
+
 
 
 if __name__ == "__main__":
